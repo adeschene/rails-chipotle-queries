a3bcba9192e480a32bfcaf97fc6190e69ce945fb
adeschene <alecdeschene@gmail.com>
2021-03-07 13:31:07 -0800

    add seeding with csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Go thru csv row by row
+IO.foreach("lib/assets/chipotle.csv") do |row|
+  # Ex: row == "1,2,taco,[meat,lettuce,cheese],$5.67"
+  description = row.slice(/\[.*\]/) # row => "[meat,lettuce,cheese]"
+  attributes  = row.split(/,\[.*\],/).join(",").split(",") # row => ["1","2","taco","$5.67"]
+
+  order_id    = attributes[0].to_i # "1" => 1
+  quantity    = attributes[1].to_i # "2" => 2
+  item_name   = attributes[2] # "taco"
+  # "$5.67" => "5.67" => 5.67 => 567.0 => 567
+  item_price  = (attributes.last.tr('$','').to_f * 100).to_i
+
+  Order.create( # Create a row in db from variables above
+    order_id: order_id,
+    quantity: quantity,
+    item_name: item_name,
+    choice_description: description.nil? ? "NULL" : description,
+    item_price: item_price
+  )
+end
